d5d9ae174179e469eea9e7f5d7b81ed419bb1459
RicardoFrieza <a71483@ualg.pt>
2025-06-04 13:14:51 +0100

    Created Level 1 with plataforms, some prefabs to help later, and other stuff

---
+++ Assets/ThirdParty/GIGA Softworks/Pixel Cable Renderer/Scripts/Vector3Extension.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GIGA.PixelCableRenderer
+{
+    public static class Vector3Extension
+    {
+        public static float Magnitude2D(this Vector3 vec)
+        {
+            return Mathf.Sqrt(vec.x * vec.x + vec.y * vec.y);
+        }
+
+		// Rotation
+
+		public static Vector3 Rotated(this Vector3 vector, Quaternion rotation, Vector3 pivot = default(Vector3))
+		{
+			return rotation * (vector - pivot) + pivot;
+		}
+
+		public static Vector3 Rotated(this Vector3 vector, Vector3 rotation, Vector3 pivot = default(Vector3))
+		{
+			return Rotated(vector, Quaternion.Euler(rotation), pivot);
+		}
+
+		public static Vector3 Rotated(this Vector3 vector, float x, float y, float z, Vector3 pivot = default(Vector3))
+		{
+			return Rotated(vector, Quaternion.Euler(x, y, z), pivot);
+		}
+	}
+}
